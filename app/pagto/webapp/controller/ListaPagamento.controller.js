23520c6731df8ace09d3612025c3822526e40304
Matheus Gimenes <matheus_gimenes0@hotmail.com>
2021-11-04 20:13:44 +0000

    feat - Create a mock list

--- app/pagto/webapp/controller/ListaPagamento.controller.js
+++ app/pagto/webapp/controller/ListaPagamento.controller.js
@@ -1,16 +1,25 @@
 // eslint-disable-next-line no-undef
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/m/MessageToast",
+    "./Formatter",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (MessageToast, Formatter, Controller,JSONModel) {
 		"use strict";
 
 		return Controller.extend("pagto.controller.ListaPagamento", {
 			onInit: function () {
-                
-			}
+                var oModel = new JSONModel(sap.ui.require.toUrl("pagto/mock/payments.json"));
+			    this.getView().setModel(oModel);
+            },
+            onListItemPress: function (oEvent) {
+			    MessageToast.show("Pressed : " + oEvent.getSource().getTitle());
+		    }
 		});
-	});
+    });
+    
+// eslint-disable-next-line no-undef
